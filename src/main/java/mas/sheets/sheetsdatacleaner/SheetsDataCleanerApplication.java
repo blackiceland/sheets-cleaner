653b66e6c1c9ef76ff67b367cdb11421d3fd0ff9
blackiceland <Dfghjc111asdR#>
2025-04-14 18:28:32 +0600

    feat(conf): Build framework

---
+++ src/main/java/mas/sheets/sheetsdatacleaner/SheetsDataCleanerApplication.java
@@ -0,0 +1,13 @@
+package mas.sheets.sheetsdatacleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsDataCleanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetsDataCleanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mas/sheets/sheetsdatacleaner/SheetsDataCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package mas.sheets.sheetsdatacleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsDataCleanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
