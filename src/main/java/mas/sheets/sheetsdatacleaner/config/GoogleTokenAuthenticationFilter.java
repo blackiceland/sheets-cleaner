a6b4b0f9e0303190071d9fb37e15ed48ba81a40b
blackiceland <jaggervid1@gmail.com>
2025-08-18 17:55:16 +0600

    Аутентификация 3.

--- src/main/java/mas/sheets/sheetsdatacleaner/config/GoogleTokenAuthenticationFilter.java
+++ src/main/java/mas/sheets/sheetsdatacleaner/config/GoogleTokenAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,7 @@ public class GoogleTokenAuthenticationFilter extends OncePerRequestFilter {
     private final GoogleTokenValidator tokenValidator;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
